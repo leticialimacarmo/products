5aeca3a7cd551835684f49d90c89f5b81e7a7a60
Letícia Lima <101489147+LETICIALIMA77@users.noreply.github.com>
2024-06-20 13:01:06 -0300

    Add files via upload

---
+++ clientes/class/itensClient.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../../common/db.php';
+
+
+try {
+  $db = new PDO($dsn, $username, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+  $query = "SELECT * FROM clients WHERE data_deleted IS NULL ORDER BY id DESC";
+  $stmt = $db->prepare($query);
+  $stmt->execute();
+  $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+  $itens = array_map(function ($item) {
+    return  [
+      'id' => $item['id'],
+      'nomeFantasia' => $item['nomeFantasia'],
+      'razaoSocial' => $item['razaoSocial'],
+      'cpf' => $item['cpf'],
+      'cidade' => $item['cidade'],
+      'data_cadastro' => $item['data_cadastro']
+    ];
+  }, $res);
+
+
+  echo json_encode(['data' =>$itens]);
+} catch (PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
