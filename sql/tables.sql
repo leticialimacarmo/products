74be087939ed60653c2d181b64c5b7a898a38aac
Letícia Lima <101489147+LETICIALIMA77@users.noreply.github.com>
2024-06-20 03:57:54 -0300

    Add files via upload

---
+++ sql/tables.sql
@@ -0,0 +1,48 @@
+CREATE TABLE
+  `clients` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `nomeFantasia` varchar(100) DEFAULT NULL,
+    `razaoSocial` varchar(100) DEFAULT NULL,
+    `cpf` varchar(100) DEFAULT NULL,
+    `cidade` varchar(100) DEFAULT NULL,
+    `data_cadastro` datetime DEFAULT current_timestamp(),
+    `data_deleted` date DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+
+CREATE TABLE
+  `products` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `codigo` varchar(100) DEFAULT NULL,
+    `descricao` varchar(100) DEFAULT NULL,
+    `valor` varchar(100) DEFAULT NULL,
+    `fornecedor` varchar(100) DEFAULT NULL,
+    `data_cadastro` datetime DEFAULT current_timestamp(),
+    `data_deleted` date DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+
+
+  CREATE TABLE
+  `sales` (
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `cliente` varchar(255) DEFAULT NULL,
+    `tipoPagamento` varchar(100) DEFAULT NULL,
+    `valorIntegral` varchar(100) DEFAULT NULL,
+    `vendedor` varchar(100) DEFAULT NULL,
+    `produto` varchar(100) DEFAULT NULL,
+    `quantidade` varchar(100) DEFAULT NULL,
+    `valorUnitario` varchar(255) DEFAULT NULL,
+    `data_cadastro` datetime DEFAULT current_timestamp(),
+    `data_deleted` date DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+
+
+  CREATE TABLE 
+  `payments` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `numero` INT NOT NULL,
+    `valorParcela` DECIMAL(10, 2) NOT NULL,
+    `dataVencimento` DATE NOT NULL
+  );
