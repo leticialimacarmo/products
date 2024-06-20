71dc264fe5e11625b005f767570457a22f2d8f4f
Letícia Lima <101489147+LETICIALIMA77@users.noreply.github.com>
2024-06-20 12:36:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,7 +26,30 @@
 <body>
 
   <main>
-    <?php require_once "./principal/index.php"; ?>
+    <div class="lateral-menu active" id="lateral-menu">
+      <header>
+
+        <nav>
+          <h5 class="logo"><i data-feather="codesandbox"></i> INTRANET</h5>
+          <a id="menu"><i class="menu" data-feather="menu"></i></a>
+        </nav>
+      </header>
+
+      <ul>
+        <li><i data-feather="archive"></i> <a href="./produtos/"><span>Produtos</span></a></li>
+        <li><i data-feather="corner-down-right"></i><a href="./vendas/"><span>Vendas</span></a></li>
+        <li><i data-feather="list"></i><a href="./listagem/"><span>Listagem de Vendas</span></a></li>
+        <li><i data-feather="user"></i><a href="./clientes/"><span>Clientes</span></a></li>
+      </ul>
+
+      <footer>
+        <div class="text">
+          <br>
+          <h5>Letícia Lima</h5>
+          <p>Desenvolvedora FullStack</p>
+        </div>
+      </footer>
+    </div>
     <div class="content">
       <div id="content">
 
@@ -82,7 +105,7 @@
 
         </nav>
 
-      
+
 
 
       </div>
